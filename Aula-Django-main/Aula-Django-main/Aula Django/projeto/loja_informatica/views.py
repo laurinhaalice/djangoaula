dc7d47cc6896203c19cbc1b12278cafe6b283e10
laura alice <lagglaura@gmail.com>
2025-01-17 20:29:11 -0300

    .

---
+++ Aula-Django-main/Aula-Django-main/Aula Django/projeto/loja_informatica/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Produto
+
+def lista_produtos(request):
+    produtos = Produto.objects.all()  # Busca todos os produtos
+    return render(request, 'loja_informatica/lista_produtos.html', {'produtos': produtos})

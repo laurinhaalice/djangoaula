59578144928c4eab3c7e8467aab194f367fbb9fe
laura alice <lagglaura@gmail.com>
2025-01-17 21:33:08 -0300

    senhor jesus
    oque é isso

--- Aula-Django-main/Aula-Django-main/Aula Django/projeto/loja_informatica/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Produto
-
-def lista_produtos(request):
-    produtos = Produto.objects.all()  # Busca todos os produtos
-    return render(request, 'loja_informatica/lista_produtos.html', {'produtos': produtos})
